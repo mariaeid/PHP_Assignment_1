23dd568e716797caf8a71b48a0bf7729958edf19
root <root@DESKTOP-2TKMD5S.localdomain>
2017-10-19 12:04:09 +0200

    Updated structure of the post-array

--- data.php
+++ data.php
@@ -3,24 +3,24 @@
 // authors.
 declare(strict_types=1);
 
-$authors = [
-    'Kalle Karlsson',
-    'Olle Olsson'
-];
-
-$titles = [
-    'Att koka soppa på en spik',
-    'Eternitplattor gör dig smalare'
-];
+$content1 = "Both the authors and the news feed items should be stored in Arrays in a structure that is simple to use in your code and that's easily understandable. The authors and news feed items don't have to be within the same Array, but some sort of connection should exist between a news feed item and it's author.";
+$content2 = "Never the less the news feed items should be ordered based on the published date, so the latest news feed item should go on top and vice versa.";
+$content3 = "Blabla";
 
 $posts = [
-    ['date' => '2017-01-01'],
-    ['date' => '2016-02-01'],
-    ['date' => '2015-01-01'],
-    ['date' => '2014-01-01']
-];
+  ['title' => 'News1',
+  'content' => $content1,
+  'author' => 'James Brown',
+  'date' => '2017-01-01'],
 
-$newsFeeds = [
-    ['title' => $titles[0], 'author' => $authors[0]],
-    ['title' => $titles[1], 'author' => $authors[1]],
+  ['title' => 'News2',
+  'content' => $content2,
+  'author' => 'Hanna Bell',
+  'date' => '2015-01-01'],
+
+  ['title' => 'News3',
+  'content' => $content3,
+  'author' => 'Annabelle',
+  'date' => '2016-01-01'],
+
 ];
