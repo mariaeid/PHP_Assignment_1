77d33330f0589c99a6e70870d997e656048cfc9e
root <root@DESKTOP-2TKMD5S.localdomain>
2017-10-27 10:18:09 +0200

    Added more blog content and elements in array posts

--- data.php
+++ data.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+//Content for blog posts
+
 $content1 = "An 'extremely credible source' has called my office and told me
 that Lorem Ipsum's birth certificate is a fraud. Despite the constant negative
 ipsum covfefe. Lorem Ipsum is unattractive, both inside and out. I fully
@@ -21,46 +23,82 @@ $content4 = "Lorem Ipsum is FAKE TEXT! The concept of Lorem Ipsum was created by
 and for the Chinese in order to make U.S. design jobs non-competitive. Look at
 that text! Would anyone use that? Can you imagine that, the text of your next
 webpage?!";
+$content5 = "I don't think anybody knows it was Russia that wrote Lorem Ipsum,
+but I don't know, maybe it was. It could be Russia, but it could also be China.
+It could also be lots of other people. It also could be some wordsmith sitting
+on their bed that weights 400 pounds. Ok? If Trump Ipsum weren’t my own words,
+perhaps I’d be dating it.";
+$content6 = "That other text? Sadly, it’s no longer a 10. The best taco bowls
+are made in Trump Tower Grill. I love Hispanics! My placeholder text, I think,
+is going to end up being very good with women. Trump Ipsum is calling for a
+total and complete shutdown of Muslim text entering your website.";
+$content7 = "Lorem Ipsum is the single greatest threat. We are not - we are not
+keeping up with other websites. We are going to make placeholder text great
+again. Greater than ever before. Look at that text! Would anyone use that? Can
+you imagine that, the text of your next webpage?!";
+$content8 = "This placeholder text is gonna be HUGE. Lorem Ipsum's father was
+with Lee Harvey Oswald prior to Oswald's being, you know, shot. The best taco
+bowls are made in Trump Tower Grill. I love Hispanics!";
+$content9 = "Lorem Ipsum's father was with Lee Harvey Oswald prior to Oswald's
+being, you know, shot. You could see there was text coming out of her eyes, text
+coming out of her wherever. Lorem Ipsum better hope that there are no 'tapes'
+of our conversations before he starts leaking to the press!";
+$content10 = "You’re disgusting. You know, it really doesn’t matter what you
+write as long as you’ve got a young, and beautiful, piece of text. I have a 10
+year old son. He has words. He is so good with these words it's unbelievable.";
+
+//Array with the blog posts. An array within the array for full author details
 
 $posts = [
+
   ['title' => 'Ipsum covfefe',
-  'likeID' => 1,
   'content' => $content1,
-  'author' => ['name' => 'Steven Mnuchin', 'ID' => 100],
-  'date' => '2014-01-01',
-  'image' => 'img/Mnuchin.png',
-  'likeCounter' => 2577],
+  'author' => ['name' => 'Steven Mnuchin', 'ID' => 101, 'image' => 'img/Mnuchin.png'],
+  'date' => '2016-12-15'],
 
   ['title' => 'We need to build a wall',
-  'likeID' => 2,
   'content' => $content2,
-  'author' => ['name' => 'Jeff Sessions', 'ID' => 101],
-  'date' => '2015-01-01',
-  'image' => 'img/Sessions.png',
-  'likeCounter' => 6498],
+  'author' => ['name' => 'Jeff Sessions', 'ID' => 102, 'image' => 'img/Sessions.png'],
+  'date' => '2017-10-03'],
 
   ['title' => 'I will write some great, great text',
-  'likeID' => 3,
   'content' => $content3,
-  'author' => ['name' => 'Donald Trump', 'ID' => 102],
-  'date' => '2016-01-01',
-  'image' => 'img/Trump.png',
-  'likeCounter' => 1467],
+  'author' => ['name' => 'Donald Trump', 'ID' => 103, 'image' => 'img/Trump.png'],
+  'date' => '2017-10-20'],
 
   ['title' => 'Fake text',
-  'likeID' => 4,
   'content' => $content4,
-  'author' => ['name' => 'Rex Tillerson', 'ID' => 103],
-  'date' => '2016-05-17',
-  'image' => 'img/Tillerson.png',
-  'likeCounter' => 1467]
+  'author' => ['name' => 'Rex Tillerson', 'ID' => 104, 'image' => 'img/Tillerson.png'],
+  'date' => '2017-05-17'],
+
+  ['title' => 'It could be Russia',
+  'content' => $content5,
+  'author' => ['name' => 'Mike Pence', 'ID' => 105, 'image' => 'img/Pence.png'],
+  'date' => '2017-10-11'],
+
+  ['title' => 'It is no longer a 10',
+  'content' => $content6,
+  'author' => ['name' => 'Mike Pence', 'ID' => 105, 'image' => 'img/Pence.png'],
+  'date' => '2016-02-07'],
+
+  ['title' => 'Greater than ever before',
+  'content' => $content7,
+  'author' => ['name' => 'Donald Trump', 'ID' => 103, 'image' => 'img/Trump.png'],
+  'date' => '2017-07-25'],
+
+  ['title' => 'It\'s gonna be HUGE',
+  'content' => $content8,
+  'author' => ['name' => 'Rex Tillerson', 'ID' => 104, 'image' => 'img/Tillerson.png'],
+  'date' => '2016-05-17'],
+
+  ['title' => 'Lorem Ipsum\'s father',
+  'content' => $content9,
+  'author' => ['name' => 'Steven Mnuchin', 'ID' => 101, 'image' => 'img/Mnuchin.png'],
+  'date' => '2015-12-31'],
+
+  ['title' => 'Beautiful piece of text',
+  'content' => $content10,
+  'author' => ['name' => 'Jeff Sessions', 'ID' => 102, 'image' => 'img/Sessions.png'],
+  'date' => '2016-10-03'],
 
 ];
-
-// $authors = [
-//   ['fullName' => 'Steven Mnuchin',
-//   'ID' => 123],
-//
-//   ['fullName' => 'Donald Trump',
-//   'ID' => 124]
-// ]
