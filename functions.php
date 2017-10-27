cd902edaa514b1ab029b4379599a09884abefff8
root <root@DESKTOP-2TKMD5S.localdomain>
2017-10-27 10:23:21 +0200

    Added comments to the code

--- functions.php
+++ functions.php
@@ -2,13 +2,12 @@
 
 declare(strict_types=1);
 
+//Function that sorts the posts array by date with latest date first
+
 function sortByDate($a, $b) {
-    return (int) $a['date'] < (int) $b['date'];
+    return strtotime ($a['date']) < strtotime ($b['date']);
 }
 
 usort($posts, 'sortByDate');
 
-
- function hello(){
-     echo "Hello";
- }
+?>
