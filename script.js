1252ba670497d08daf887b3ff606c8b9dd875327
root <root@DESKTOP-2TKMD5S.localdomain>
2017-10-30 08:00:16 +0100

    Added comments to the code

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 'use strict';
 
-// Creates constants of the like_button and like_input from html
+// Loops through all bottons and assign each of them a function that will
+// increase their respective like counter by one when someone clicks on them.
 const like_buttons = document.querySelectorAll(".like_button");
 const like_inputs = document.querySelectorAll(".like_input");
-
 for (let i=0; i<like_buttons.length; i++) {
   const button = like_buttons[i];
-  //Each time the like_button is clicked the value of like_input is increased by one
   button.addEventListener('click', function() {
     let like = like_inputs[i].value;
     let new_like = parseInt(like) + 1;
